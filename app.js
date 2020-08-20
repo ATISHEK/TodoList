e20a61f05e151f0cfaa7030c357694bdd66c800a
ATISHEK <44174149+ATISHEK@users.noreply.github.com>
2020-08-21 00:37:33 +0530

    second commit

--- app.js
+++ app.js
@@ -136,8 +136,9 @@ app.get("/about", function(req, res) {
 
 
 
+let port = process.env.PORT ;
+if(port==null || port =="")port = 3000;
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started");
 });
